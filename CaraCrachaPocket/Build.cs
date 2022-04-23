a907edfe4ae1143bbe3d352f401ada6999f18d02
Guilherme <guilhergoa@hotmail.com>
2022-04-23 20:51:26 -0300

    :ok_hand: File refactored

--- CaraCrachaPocket/Build.cs
+++ CaraCrachaPocket/Build.cs
@@ -8,8 +8,8 @@ namespace CaraCrachaPocket;
 
 public class Build : IDocument
 {
-    private readonly float baseOffsetWidth = PageSizes.A5.Landscape().Width / 10;
-    private readonly float baseOffsetHeight = PageSizes.A5.Landscape().Height / 10;
+    private readonly float baseOffsetWidth = PageSizes.A5.Landscape().Width / 20;
+    private readonly float baseOffsetHeight = PageSizes.A5.Landscape().Height / 20;
     
     public DocumentMetadata GetMetadata() => DocumentMetadata.Default;
 
@@ -21,99 +21,167 @@ public class Build : IDocument
                 page.Size(PageSizes.A5.Landscape());
                 page.Background(Colors.White);
 
-                page.Header().Element(ComposeHeader);
                 page.Content().Element(ComposeContent);
-                page.Footer().Element(ComposeFooter);
             });
     }
-
-    private void ComposeHeader(IContainer container)
+    
+    private void ComposeContent(IContainer container)
     {
-        container.Layers(layers =>
+        container.Border(4)
+            .BorderColor("#afafaf")
+            .Column(columns =>
         {
-            // layer header
-            layers.PrimaryLayer().Canvas((canvas, space) =>
+            columns.Item()
+                .Width(0)
+                .Height(0)
+                .Layers(layers =>
             {
-                using var paint = new SKPaint
+                layers.PrimaryLayer().Canvas((canvas, space) =>
                 {
-                    Color = SKColor.Parse("#18202f"),
-                };
+                    using var paint = new SKPaint
+                    {
+                        Color = SKColor.Parse("#cbdbe3"),
+                    };
+                    
+                    canvas.DrawRoundRect(baseOffsetWidth * 11, baseOffsetHeight * 6, baseOffsetWidth * 10,
+                        baseOffsetHeight * 10, 30, 30, paint);
+                });
+            });
 
-                canvas.DrawRect(0, 0, baseOffsetWidth * 10, baseOffsetHeight * 4, paint);
+            //  header layer
+            columns.Item()
+                .Width(baseOffsetWidth *12)
+                .Height(baseOffsetHeight * 5)
+                .Layers(layers =>
+            {
+                layers.PrimaryLayer().Canvas((canvas, space) =>
+                {
+                    using var paint = new SKPaint
+                    {
+                        Color = SKColor.Parse("#18202f"),
+                    };
+
+                    canvas.DrawRect(0, 0, baseOffsetWidth * 20, baseOffsetHeight * 8, paint);
+                });
+
+                // write header layer
+                layers.Layer()
+                    .TranslateX(baseOffsetWidth)
+                    .TranslateY(baseOffsetHeight / 2 + 20)
+                    .Width(baseOffsetWidth * 10 + 30)
+                    .Text("Digite aqui seu nome e sobrenome.")
+                    .FontSize(22)
+                    .FontColor("#00c0d0")
+                    .Bold();
+                    
+
+                layers.Layer()
+                    .TranslateX(baseOffsetWidth )
+                    .TranslateY(baseOffsetHeight * 3 + 20)
+                    .Width(baseOffsetWidth * 10 + 30)
+                    .Text("Digite aqui a cidade/estado que mora.")
+                    .FontSize(20)
+                    .FontColor(Colors.White)
+                    .Bold();
+
+                layers.Layer()
+                    .TranslateX(baseOffsetWidth)
+                    .TranslateY(baseOffsetHeight * 5 + 20)
+                    .Width(baseOffsetWidth * 10 + 30)
+                    .Text("Digite aqui a area que ira trabalhar.")
+                    .FontSize(16)
+                    .FontColor(Colors.White);
             });
 
             // Line layer
-             layers.Layer().Canvas(((canvas, space) =>
-             {
-                 using var paint = new SKPaint
-                 {
-                     Color = SKColor.Parse("#f05480"),
-                     StrokeWidth = 3
-                 };
+            columns.Item()
+                .Width(0)
+                .Height(0)
+                .TranslateY(- baseOffsetHeight * 5)
+                .Layers(layers =>
+            {
+                layers.PrimaryLayer().Canvas((canvas, space) =>
+                {
+                    using var paint = new SKPaint
+                    {
+                        Color = SKColor.Parse("#f05480"),
+                        StrokeWidth = 4,
+                    };
+                    
+                    canvas.DrawLine(baseOffsetWidth / 2, 0, baseOffsetWidth / 2, baseOffsetHeight * 10, paint);
+                });
+            });
+            
+            // text layer
+            columns.Item()
+                .Width(baseOffsetWidth * 9)
+                .Height(baseOffsetHeight * 5)
+                .Layers(layers =>
+            {
+                layers.PrimaryLayer().Canvas(((canvas, space) =>
+                    {
+                        using var paint = new SKPaint
+                        {
+                            Color = SKColor.Parse("#caeff0"),
+                            IsStroke = true,
+                            StrokeWidth = 2,
+                        };
+                    
+                        canvas.DrawRect(baseOffsetWidth, baseOffsetHeight * 4, baseOffsetWidth * 8 + 40, baseOffsetHeight * 4, paint);
+                        canvas.DrawRect(baseOffsetWidth, baseOffsetHeight * 9, baseOffsetWidth * 8 + 40, baseOffsetHeight * 2, paint);
+                        canvas.DrawRect(baseOffsetWidth, baseOffsetHeight * 12, baseOffsetWidth * 8 + 40, baseOffsetHeight * 2, paint);
+                    }));
+                
+                // write text layer
+                layers.Layer()
+                    .TranslateX(baseOffsetWidth + 10)
+                    .TranslateY(baseOffsetHeight * 4)
+                    .Width(baseOffsetWidth * 8 + 20)
+                    .Height(baseOffsetHeight * 4)
+                    .Text("Digite aqui um fato curioso da sua vida que gostaria de compartilhar com a gente!")
+                    .FontSize(12)
+                    .FontColor(Colors.Black)
+                    .Bold();
+                
+                layers.Layer()
+                    .TranslateX(baseOffsetWidth + 10)
+                    .TranslateY(baseOffsetHeight * 9)
+                    .Width(baseOffsetWidth * 8 + 20)
+                    .Height(baseOffsetHeight * 2)
+                    .Text("Digite aqui se passatempo favorito.")
+                    .FontSize(12)
+                    .FontColor(Colors.Black);
+                
+                layers.Layer()
+                    .TranslateX(baseOffsetWidth + 10)
+                    .TranslateY(baseOffsetHeight * 12)
+                    .Width(baseOffsetWidth * 8 + 20)
+                    .Height(baseOffsetHeight * 2)
+                    .Text("Digite aqui estilo de musica, filmes e jogos de sua preferencia.")
+                    .FontSize(12)
+                    .FontColor(Colors.Black);
 
-                 canvas.DrawLine(baseOffsetWidth / 2,  0, baseOffsetWidth / 2, baseOffsetHeight * 6, paint);
-             }));
+                layers.Layer()
+                    .TranslateX(baseOffsetWidth * 12 + 20)
+                    .TranslateY(baseOffsetHeight * 7)
+                    .Width(baseOffsetWidth * 6)
+                    .Height(baseOffsetHeight * 5)
+                    .AlignCenter()
+                    .Text("Deixe aqui uma mensagem de até 10 palavras pra os Take.seres!")
+                    .FontSize(16)
+                    .FontColor(Colors.Black);
+            });
 
-            // // user layer
-            //  layers.Layer().Canvas(((canvas, space) =>
-            //  {
-            //      using var paint = new SKPaint
-            //      {
-            //          Color = SKColor.Parse("#07c1d1"),
-            //      };
-            //
-            //      canvas.DrawRect(space.Width - 220,  0, space.Width / 4 + 50, (space.Height / 3) * 2 - 50, paint);
-            //  }));
-            //  
-            //  //Image layers
-            //
-            //  byte[] imageData = File.ReadAllBytes("../../../images/photo.png");
-            //  layers.Layer().Image(imageData, ImageScaling.FitWidth);
-
-
-            // write header layer
-            layers.Layer()
-                .PaddingLeft(60)
-                .PaddingTop(20)
-                .Text("Digite aqui seu nome e sobrenome")
-                .FontSize(16).FontColor("#00c0d0")
-                .Bold();
-
-            layers.Layer()
-                .PaddingLeft(60)
-                .PaddingTop(60)
-                .Text("Digite aqui a cidade/estado que mora")
-                .FontSize(16).FontColor(Colors.White)
-                .Bold();
-
-            layers.Layer()
-                .PaddingLeft(60)
-                .PaddingTop(100)
-                .Text("Digite aqui a area que ira trabalhar")
-                .FontSize(16).FontColor(Colors.White);
+            //User image layer
+            byte[] imageData = File.ReadAllBytes("../../../images/photo.png");
+            columns.Item()
+                .TranslateX(baseOffsetWidth * 13 - 30)
+                .TranslateY(-baseOffsetHeight * 9)
+                .Width(baseOffsetWidth * 7)
+                .Height(baseOffsetHeight * 10)
+                .Border(2)
+                .BorderColor("#0eb7c5")
+                .Image(imageData, ImageScaling.Resize);
         });
-    }
-
-    private void ComposeContent(IContainer container)
-    {
-        //layers de escrita de 10 palavras
-        container.Layers(layers =>
-        {
-             layers.PrimaryLayer().Canvas(((canvas, space) =>
-             {
-                 using var paint = new SKPaint
-                 {
-                     Color = SKColor.Parse("#cbdbe3"),
-                 };
-
-                 canvas.DrawRoundRect(baseOffsetWidth * 6 ,  -baseOffsetHeight * 7, baseOffsetWidth * 5, 
-                     baseOffsetHeight * 4, 30, 30, paint);
-             }));
-        });
-    }
-
-    private void ComposeFooter(IContainer container)
-    {
-       
     }
 }
